1c1cae078ed3e7e7852048198822b0d3deb99a1a
Divang2005 <2023pietcadivang016@poornima.org>
2024-12-28 01:49:57 +0530

    files are update

--- leetcode/494.cpp
+++ leetcode/494.cpp
@@ -8,21 +8,16 @@ class Solution
 public:
     int findTargetSumWays(vector<int> &nums, int target)
     {
-        // Calculate the total sum of nums
+
         int sum = accumulate(nums.begin(), nums.end(), 0);
 
-        // If the target is not reachable, return 0
         if (abs(target) > sum || (sum + target) % 2 != 0)
         {
             return 0;
         }
-
-        // Calculate the subset sum we need to find
         int subsetSum = (sum + target) / 2;
 
-        // Use dynamic programming to count subsets that sum to subsetSum
         vector<int> dp(subsetSum + 1, 0);
-        dp[0] = 1; // Base case: There's one way to get a sum of 0 (by taking no elements)
 
         for (int num : nums)
         {
