6f575984cc10b2ea8c7a34e831929b5b00d61052
Divang2005 <2023pietcadivang016@poornima.org>
2025-03-18 19:04:05 +0530

    new

---
+++ leetcode/march/2161.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+class Solution {
+    public:
+     vector<int> pivotArray(vector<int>& nums, int pivot) {
+       vector<int> ans;
+   
+       for (const int num : nums)
+         if (num < pivot)
+           ans.push_back(num);
+   
+       for (const int num : nums)
+         if (num == pivot)
+           ans.push_back(num);
+   
+       for (const int num : nums)
+         if (num > pivot)
+           ans.push_back(num);
+   
+       return ans;
+     }
+   };
